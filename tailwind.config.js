4ad073bf16d87ac386becb7775444f747c8b3c9a
Nicolas Rojo <nicolas.martin.rojo@gmail.com>
2020-10-28 00:13:01 -0300

    Install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#9B51E0",
+        secondary: "#FE7360",
+        tertiary: "#F2F2F2",
+        gradients: {
+          primary: {
+            1: "#7129E4",
+            2: "#9B51E0",
+          },
+          secondary: {
+            1: "#FE7360",
+            2: "#EB5757",
+          },
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
